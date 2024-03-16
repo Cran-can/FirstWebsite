9e19be84848f3e24da99f9ce51f97f7495bd31ae
Gnaneshwar <aareddy.gnaneshwar@capgemini.com>
2024-03-17 02:28:44 +0530

    first

---
+++ process.php
@@ -0,0 +1,13 @@
+<?php
+
+      if(isset($_POST{'submmit'})){
+        
+        $to = "1243cran@gmail.com";
+        $subject = "My subject"; 
+        $txt = "Hello world!";
+        $headers = "From: webmaster@example.com" . "\r\n" ."CC: somebodyelse@example.com";
+
+        mail($to,$subject,$txt,$headers);
+      }
+
+?>
